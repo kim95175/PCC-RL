7ee32cb402a369970c823a0d4898abfd70c9ee06
kim95175 <kim95175@gmail.com>
2020-01-30 17:31:56 +0900

    add default model path for testing by pantheon

--- src/udt-plugins/testing/loaded_client.py
+++ src/udt-plugins/testing/loaded_client.py
@@ -40,7 +40,9 @@ RESET_RATE_MAX = 100.0
 RESET_RATE_MIN = 6.0
 RESET_RATE_MAX = 6.0
 
-MODEL_PATH = arg_or_default("--model-path", "/tmp/")
+#MODEL_PATH = arg_or_default("--model-path", "/tmp/")
+MODEL_PATH = arg_or_default("--model-path", "/home/airman/Github/PCC-RL/src/icml_paper_model/")
+
 
 for arg in sys.argv:
     arg_str = "NULL"
